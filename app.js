b06bc0562d67dfc93f4ab795190bc47a2793db1e
styp3r <stypertypes@gmail.com>
2021-06-30 17:55:54 +0530

    add body-parser from npm and parse JSON from api to get lat long

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const place = require("random-places");
 
@@ -9,7 +10,29 @@ var country = "";
 
 app.get("/", (req, res)=>{
   city = getRandomPlace().name;
-  country = getRandomPlace().country;
+
+  const https = require("https");
+
+  https.get("https://us1.locationiq.com/v1/search.php?key=pk.d2e2d871f765cc0203dc4423965f3d41&q="+city+"&format=json", (res) => {
+  console.log('statusCode:', res.statusCode);
+  var data;
+    res.on("data", function(chunk) {
+      if (!data) {
+        data = chunk;
+      } else {
+        data += chunk;
+      }
+    });
+
+  res.on('end', () => {
+    const obj = JSON.parse(data);
+    console.log(obj[0].lat);
+    console.log(obj[0].lon);
+  });
+
+}).on('error', (e) => {
+  console.error(e);
+});
 
 
 //https://us1.locationiq.com/v1/search.php?key=pk.d2e2d871f765cc0203dc4423965f3d41&q=Empire%20State%20Building&format=json
